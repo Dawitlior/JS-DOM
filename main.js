ce1c11c07d3fde6bca95c38836d0b3e84d210d2e
Dawitlior <dawitlior777@gmail.com>
2022-05-29 21:58:36 +0300

    add

---
+++ main.js
@@ -0,0 +1,73 @@
+// 1.	צרו אלמנט של טקסט ב html עם סטרינג "first dom app"  והדפיסו ללוג את התוכן שלו.
+// first_id
+
+
+
+// 2.	צרו אלמנט טקסט ב html בלי טקסט וכתבו לתוכו "my dom app" בעזרת JS.
+// document.write(h6_id.textContent + "my dom app")
+
+
+// 3.	צרו 4 אלמנטי טקסט מסוג שונה ב html עם טקסטים שונים והדפיסו אותם ללוג.
+// var someVar = document.getElementsByClassName("targil3");
+// console.log(someVar);
+
+// 4.	הדפיסו ללוג את האלמנט השני.
+// 5.	הדפיסו ללוג את הטקסט של האלמנט השלישי.
+// var someVar1 = someVar[1];
+// var someVar2 = someVar[2];
+
+
+// 6.	צרו 4 אלמנטים מאותו סוג ב html עם תוכן שונה והדפיסו אותם ללוג.
+// var targil6 = document.getElementsByClassName("someSpan");
+
+
+// 7.	צרו פונקציה המקבלת מהמשתמש שם והדפיסו אותו למסך בתגית H2.
+// function funkyFunction(){
+// var userInput = prompt("type some name");
+// document.write("<h2>" + userInput + "</h2>")
+// }
+// funkyFunction();
+
+// 8.	צרו פונקציה המקבלת מהמשתמש צבע ומספר, הדפיסו למסך את הצבע בתגית SPAN מספר הפעמים שהמשתמש הכניס.
+
+// function getInputFromUser(){
+//     var userRoundPrompt = prompt("type a number");
+//     for(var i = 0 ; i <userRoundPrompt ; i++){
+//         var color = prompt("type some color");
+//         // var number = +prompt("type some number");
+//     }
+//     document.write("<br>" +"<span>" + color + "</span>")
+
+
+// }
+// getInputFromUser();
+
+
+// 9.	צרו פונקציה המקבלת מהמשתמש טקסט ושם תגית ואז בונה את התגית עם הטקסט ב DOM.
+
+function getFromUserAndBuild(){
+var textFromUser = prompt("type some text");
+var nameTag = prompt("type random Tag name");
+
+
+}
+getFromUserAndBuild();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
