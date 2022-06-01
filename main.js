c8e829f55cc44779ab8742834e20939ed0b1d71d
Dawitlior <dawitlior777@gmail.com>
2022-06-01 08:54:55 +0300

    add

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 // 1.	צרו אלמנט של טקסט ב html עם סטרינג "first dom app"  והדפיסו ללוג את התוכן שלו.
 // first_id
 
+
+
 // 2.	צרו אלמנט טקסט ב html בלי טקסט וכתבו לתוכו "my dom app" בעזרת JS.
 // document.write(h6_id.textContent + "my dom app")
+
+
 
 // 3.	צרו 4 אלמנטי טקסט מסוג שונה ב html עם טקסטים שונים והדפיסו אותם ללוג.
 // var someVar = document.getElementsByClassName("targil3");
 // console.log(someVar);
+
+
+
 
 // 4.	הדפיסו ללוג את האלמנט השני.
 // 5.	הדפיסו ללוג את הטקסט של האלמנט השלישי.
 // var someVar1 = someVar[1];
 // var someVar2 = someVar[2];
 
+
+
+
+
 // 6.	צרו 4 אלמנטים מאותו סוג ב html עם תוכן שונה והדפיסו אותם ללוג.
 // var targil6 = document.getElementsByClassName("someSpan");
+
+
+
+
 
 // 7.	צרו פונקציה המקבלת מהמשתמש שם והדפיסו אותו למסך בתגית H2.
 // function funkyFunction(){
@@ -22,6 +37,9 @@
 // document.body.innerHTML = "<h2>" + userInput + "</h2>";
 // }
 // funkyFunction();
+
+
+
 
 // 8.	צרו פונקציה המקבלת מהמשתמש צבע ומספר, הדפיסו למסך את הצבע בתגית SPAN מספר הפעמים שהמשתמש הכניס.
 
@@ -50,12 +68,11 @@
 
 // 10.	צרו פונקציה המקבלת מהמשתמש סוג קלט ויוצרת את הקלט המתאים ב DOM.
 
-function creatTheCorrectInput(){
-var inputType = prompt("write something");
-document.innerHTML = "<input type =" + inputType + "/>"
-}
-creatTheCorrectInput();
-
+// function creatTheCorrectInput(){
+// var inputType = prompt("write something");
+// document.innerHTML = "<input type =" + inputType + "/>"
+// }
+// creatTheCorrectInput();
 
 
 
@@ -121,28 +138,28 @@ creatTheCorrectInput();
 
 // 15.	צרו אובייקט של עובד, בעזרת קלטים צרו שדות של שם פרטי, שם משפחה, אימייל ומחלקה. ותנו להם ערכים מהמשתמש, הדפיסו את האובייקט למסך, כל ערך באלמנט משלו.
 
-// var employees = {};
-// employees.firstName = prompt("type your first name");
-// employees.lastname = prompt("type your last name");
-// employees.email = prompt("type your email");
-// employees.department = prompt("type your departmant");
+var employees = {};
+employees.firstName = prompt("type your first name");
+employees.lastname = prompt("type your last name");
+employees.email = prompt("type your email");
+employees.department = prompt("type your departmant");
 
-// document.body.innerHTML =
-//   "<p>" +
-//   employees.firstName +
-//   "</p>" +
+document.body.innerHTML =
+  "<p>" +
+  employees.firstName +
+  "</p>" +
 
-//   "<h6>" +
-//   employees.lastname +
-//   "</h6>" +
+  "<h6>" +
+  employees.lastname +
+  "</h6>" +
 
-//   "<span>" +
-//   employees.email +
-//   "</span>" +
+  "<span>" +
+  employees.email +
+  "</span>" +
 
-//   "<h3>" +
-//   employees.department +
-//   "</h3>";
+  "<h3>" +
+  employees.department +
+  "</h3>";
 
 
 
@@ -154,36 +171,33 @@ creatTheCorrectInput();
 
 // 16.	צרו פונקציה הקולטת מספר מהמשתמש ומייצרת מספר אובייקטים, על פי הקלט, של בניין, לכל בניין יש שם קבלן, שם חברה, מספר קומות ומספר דירות בקומה. תנו להם ערכים מהמשתמש, הדפיסו כל אובייקט למסך, כל אובייקט באלמנט משלו.
 
-// function creatSomeObjectAndPrint() {
-//   var userNumber = prompt("type a number");
-//   for (var i = 0; i < userNumber; i++) {
-//     var building = {};
-//     building.boss = prompt("type the name of the boss");
-//     building.company = prompt("type name of company");
-//     building.severalFloors = prompt("type the Several floors");
-//     building.numberOfApartments = prompt(
-//       "type the number of the apartmant in every floor"
-//     );
-//   }
-//   document.body.innerHTML =
-//     "<h2>" +
-//     building.boss +
-//     "</h2>" +
-//     " " +
-//     "<h3>" +
-//     building.company +
-//     "</h3>" +
-//     "" +
-//     "<h4>" +
-//     building.severalFloors +
-//     "</h4>" +
-//     "" +
-//     "<h5>" +
-//     building.numberOfApartments +
-//     "</h5>";
-// }
-// creatSomeObjectAndPrint();
-
+function creatSomeObjectAndPrint() {
+  var userNumber = prompt("type a number");
+  for (var i = 0; i < userNumber; i++) {
+    var building = {};
+    building.boss = prompt("type the name of the boss");
+    building.company = prompt("type name of company");
+    building.severalFloors = prompt("type the Several floors");
+    building.numberOfApartments = prompt("type the number of the apartmant in every floor");
+  }
+  document.body.innerHTML +=
+    "<h2>" +
+    building.boss +
+    "</h2>" +
+    " " +
+    "<h3>" +
+    building.company +
+    "</h3>" +
+    " " +
+    "<h4>" +
+    building.severalFloors +
+    "</h4>" +
+    " " +
+    "<h5>" +
+    building.numberOfApartments +
+    "</h5>";
+}
+creatSomeObjectAndPrint();
 
 
 
@@ -192,21 +206,29 @@ creatTheCorrectInput();
 
 // 17.	צרו פונקציה הקולטת מספר מהמשתמש ומייצרת מספר אובייקטים, על פי הקלט, של חנות, לכל חנות יש שם החנות, כתובת, מספר מחלקות ומספר עובדים. תנו להם ערכים מהמשתמש, הדפיסו רק למסך רק חנוית עם מספר עובדים גבוה מ 10, כל אובייקט באלמנט משלו.
 
-// function printTheObjects() {
-//   var userNumber = prompt("type some random number");
-//   for (var i = 0; i < userNumber; i++) {
-//     var store = {};
-//     store.name = prompt("type the name of the store");
-//     store.adress = prompt("type the adress of the store");
-//     store.numberDepartments = prompt("type how departments they have");
-//     store.workers = prompt("How many employees do they have");
-//     if (store.workers > 10) {
-//       document.body.innerHTML +=
-//         "<h6>" + store.name + "</h6>" + " " + "<h3>" + store.workers + "</h3>";
-//     }
-//   }
-// }
-// printTheObjects();
+function printTheObjects() {
+  var userNumber = prompt("type some random number");
+  for (var i = 0; i < userNumber; i++) {
+    var store = {};
+    store.name = prompt("type the name of the store");
+    store.adress = prompt("type the adress of the store");
+    store.numberDepartments = prompt("type how departments they have");
+    store.workers = prompt("How many employees do they have");
+    if (store.workers > 10) {
+      document.body.innerHTML +=
+        "<h6>" + store.name + "</h6>" + " " + "<h3>" + store.workers + "</h3>";
+    }
+  }
+}
+printTheObjects();
+
+
+
+
+
+
+
+
 
 // 18.	צרו פונקציה שמדפיסה ללוג כמה אלמנטים נוצרו במסך בפונקציה הקודמת (סעיף 17).
 
@@ -221,21 +243,21 @@ creatTheCorrectInput();
 
 // 19.	צרו רשימה ב html וצרו פונקציה הקולטת מספר מהמשתמש ומייצרת מספר אובייקטים, על פי הקלט, של איש קשר, לכל איש קשר יש שם מלא, חברה, טלפון, נייד ואימייל. תנו להם ערכים מהמשתמש, הדפיסו כל שם של איש קשר לרשימה
 
-// function someFunction(){
-// var numOfContecs = +prompt();
-// for(var i = 0 ; i<numOfContecs ; i++){
-// var contactObject = {
-//   fullName: prompt(""),
-//   company: prompt(""),
-//   phone: +prompt(""),
-//   mobile: prompt(""),
-//   email: prompt("")
-// };
-// document.getElementById("myList").innerHTML += "<li>" + contactObject.fullName + "<li>"
-// }
+function someFunction(){
+var numOfContecs = +prompt();
+for(var i = 0 ; i<numOfContecs ; i++){
+var contactObject = {
+  fullName: prompt(""),
+  company: prompt(""),
+  phone: +prompt(""),
+  mobile: prompt(""),
+  email: prompt("")
+};
+document.getElementById("myList").innerHTML += "<li>" + contactObject.fullName + "<li>"
+}
 
-// }
-// someFunction();
+}
+someFunction();
 
 // function printOnlyIfOverSomeAge() {
 //   var theUser = {};
